1fbec151fb8970d102f47afd30ed9f1c5e3293cb
Nick Dyer <nick.dyer@atos.net>
2015-06-03 10:37:42 +0100

    Writes to comments.json, renders posts before sending to server for optimisation

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/comments.json', function(req, res) {
   fs.readFile('comments.json', function(err, data) {
     var comments = JSON.parse(data);
     comments.push(req.body);
-    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 2), function(err) {
       res.setHeader('Content-Type', 'application/json');
       res.setHeader('Cache-Control', 'no-cache');
       res.send(JSON.stringify(comments));
